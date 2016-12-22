bc4446985c782480f1540d3d1d8a73e24dbe11a6
anewryzm <anewryzm@gmail.com>
2016-12-22 17:16:03 -0500

    webpack config modified with css and style loader and extract-text-plugin

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import 'zone.js/dist/zone';
-
+import './styles.css';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
